e65b0ca9376ca1d83ec3338915081720ab1d556a
sarah <sarachamorro@gmail.com>
2020-01-08 12:54:12 -0300

    Add users to users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,19 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'User successfully created'
+      redirect_to @user
+    else
+      flash.now[:error] = 'Error saving new user :-('
+      render 'new'
+    end
   end
 
   def show
@@ -12,14 +22,30 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
-  def edit
+  def destroy
+    @user = User.find(params[:id])
+    if @user&.destroy
+      flash[:success] = 'User successfully deleted'
+      redirect_to users_path
+    else
+      flash[:error] = 'Error saving deleting user :-('
+      redirect_to users_path
+    end
   end
 
-  def destroy
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
 end
