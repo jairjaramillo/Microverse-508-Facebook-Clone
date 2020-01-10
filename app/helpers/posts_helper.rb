8f7481c5a61ef9c5b6dce22b7159123b8893ed25
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-01-10 10:06:55 -0600

    Fix a linter suggestion

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,7 @@ module PostsHelper
   def who_likes(show_likes, post)
     if show_likes
       content_tag(:h2, 'People who liked this:', id: 'likers') +
-      (render partial: 'likes/like', collection: post.likes, as: :user)
+        (render partial: 'likes/like', collection: post.likes, as: :user)
     else
       ''
     end
