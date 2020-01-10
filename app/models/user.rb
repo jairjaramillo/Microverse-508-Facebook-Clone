142427c7021d4feb465f06ed7442ce8c8fcc472b
sarah <sarachamorro@gmail.com>
2020-01-10 14:18:50 -0300

    Add Friendship model and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,13 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, foreign_key: 'post_id', source: :user
 
   has_many :initiated_friendships, foreign_key: 'sender_id', class_name: 'Friendship', source: :sender
-  has_many :accepted_friendships, foreign_key: 'receiver_id', class_name: 'Friendship', source: :receiver
+  has_many :requested_friendships, foreign_key: 'receiver_id', class_name: 'Friendship', source: :receiver
 
   validates :first_name, presence: true
   validates :last_name, presence: true
 
   def friendships
-    (initiated_friendships + accepted_friendships).uniq
+    (initiated_friendships + requested_friendships).uniq
   end
 
   private
