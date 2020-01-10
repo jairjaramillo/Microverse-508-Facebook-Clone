f2a0bf67cf98e9c5bac915ccd1cd3c159796c575
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-01-10 10:04:49 -0600

    Refactor concat in helpers collection rendering

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module CommentsHelper
-  def comment_list(comments)
-    content_tag :div do
-      comments.collect do |comment|
-        concat(render('comments/comment', comment: comment))
-      end
-    end
-  end
-
   def delete_comment(comment)
     return if comment.commenter != current_user
 
